0ec06f19c09ff6a21201825464a03351f71a6265
Nodari Kobalia <qobalia8@gmail.com>
2026-05-31 22:14:26 +0400

    update app.py

--- app/app.py
+++ app/app.py
@@ -45,7 +45,7 @@ def home():
     app_version = os.environ.get('APP_VERSION', '1.0.0')
     environment = os.environ.get('ENVIRONMENT', 'development')
     return jsonify({
-        'message': 'AutoDeploy Lab is running on Oracle Cloud',
+        'message': 'AutoDeploy Lab is running',
         'version': app_version,
         'environment': environment
     })
